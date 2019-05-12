b47dcb6cd85c584f5570879e93529a3d66ae7c35
Yazin <yazin@verify.as>
2019-05-12 08:54:34 +0300

    Rename to commentary

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
-// Youtube script
-let ac = {
-  video: document.getElementsByClassName('html5-main-video')[0],
-  frames: [],
-  absoluteTime: null, /* unix timestamp at start of first frame */
+(function() {
+  // Youtube script
+  let commentary = {
+    video: document.getElementsByClassName('html5-main-video')[0],
+    frames: [],
+    startTime: null, /* unix timestamp at start of first frame */
 
-  /**
-   * Creates a new frame from the current position in the video, using
-   * the current absolute time
-   */
-  createFrame: function() {
-    this.frames.push({ 
-      beginAt: ac.video.currentTime,
-      start: ac.timeLapsed(),
-      stop: null // set later when stopped/seeking/ended
-    })
-  },
+    /**
+     * Creates a new frame from the current position in the video, using
+     * the current absolute time
+     */
+    addFrame: function() {
+      this.frames.push({ 
+        currentVideoTime: this.video.currentTime,
+        start: this.timeSinceStart(),
+        stop: null // set later when stopped/seeking/ended
+      })
+    },
 
-  /**
-   * Updates the stop time for the last frame with the current absolute
-   * timestamp
-   */
-  stopLastFrame: function() {
-    let lastFrame = this.frames[this.frames.length - 1]
-    lastFrame.stop = this.timeLapsed()
-  },
+    /**
+     * Updates the stop time for the last frame with the current absolute
+     * timestamp
+     */
+    stopLastFrame: function() {
+      let lastFrame = this.frames[this.frames.length - 1]
+      lastFrame.stop = this.timeSinceStart()
+    },
 
-  /**
-   * @returns Number of milliseconds passed since absoluteTime (our absolute
-   * time reference)
-   */
-  timeLapsed: function() {
-    return Date.now() - this.absoluteTime
-  },
-}
-
-ac.video.addEventListener('play', (e) => {
-  if(ac.frames.length == 0) ac.absoluteTime = Date.now() // mark reference time
-
-  ac.createFrame()
-})
-
-ac.video.addEventListener('pause', (e) => ac.stopLastFrame())
-ac.video.addEventListener('ended', (e) => ac.stopLastFrame())
-ac.video.addEventListener('seeked', (e) => {
-  ac.stopLastFrame()
-
-  if(!ac.video.paused) {
-    // Add a new frame from the current position, since we're still playing
-    ac.createFrame()
+    /**
+     * @returns Number of milliseconds passed since startTime (our absolute
+     * time reference)
+     */
+    timeSinceStart: function() {
+      return Date.now() - this.startTime
+    },
   }
-})
+
+  commentary.video.addEventListener('playing', (e) => {
+    if(commentary.frames.length == 0) {
+      commentary.startTime = Date.now() // mark reference time
+    }
+  
+    // Create a new frame
+    commentary.addFrame()
+  })
+  
+  ['pause', 'ended', 'seeked'].forEach(
+    (trigger) => {
+      commentary.video.addEventListener(trigger, (e) => commentary.stopLastFrame())
+    })
+})()
 
 /**
  * TODO:
